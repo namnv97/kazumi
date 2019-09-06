235ec2fa478d8b71b677dcd229432064125d064d
namnv97 <n4m.nv.1997@gmail.com>
2019-09-06 13:39:57 +0700

    update code

--- resources/views/layouts/client/search.blade.php
+++ resources/views/layouts/client/search.blade.php
@@ -1,8 +1,8 @@
 <div class="search bg-grey">
 	<div class="Search__SearchBar">
-		<div class="Search__Form">
+		<form class="Search__Form">
 			<input type="search" class="Search__Input Heading" placeholder="Search...">
-		</div>
+		</form>
 
 		<button class="Search__Close Link Link--primary" data-action="close-modal"><svg class="Icon Icon--close" role="presentation" viewBox="0 0 16 14">
 			<path d="M15 0L1 14m14 0L1 0" stroke="currentColor" fill="none" fill-rule="evenodd"></path>
@@ -14,68 +14,43 @@
 				<div class="PageLayout__Section">
 					<div class="Segment">
 						<div class="Segment__Title Segment__Title--flexed">
-							<span class="Heading Text--subdued u-h7">37 results</span>
-							<a class="Heading Link Link--secondary u-h7" href="#">View all</a>
+							<span class="Heading Text--subdued u-h7 total_result_search">{{count($pros)}} kết quả</span>
+							<a class="Heading Link Link--secondary u-h7" href="#">Xem tất cả</a>
 						</div>
 						<div class="Segment__Content">
-							<div class="row">
+							<div class="row result_search" >
+								@foreach($pros as $key => $value)
 								<div class="col-md-4 col-sm-4 col-xs-12">
 									<div class="product-item">
 										<div class="pro-img">
 											<a href="#">
-												<img src="https://cdn.shopify.com/s/files/1/0250/1519/products/esqido-accessories-eyelash-companion-glue-open_400x.jpg?v=1552317290" alt="">
+												<img src="{{$value->gallery[0]->url}}" alt="">
 												<div class="img-hide">
-													<img src="https://cdn.shopify.com/s/files/1/0250/1519/products/esqido-accessories-eyelash-companion-glue_900x.jpg?v=1552317290" alt="">
+													<?php $a = 0; ?>
+													<img src="{{$a}}" alt="">
 												</div>
 											</a>
 										</div>
 										<div class="info-product">
-											<h3 class="title-pro"><a href="#">COMPANION EYELASH GLUE</a></h3>
-											<span class="price">FROM <span>$22.00 USD</span></span>
+											<h3 class="title-pro"><a href="#">{{$value->name}}</a></h3>
+											<?php $sale = $value->price()->sale ? $value->price()->sale : ""; ?>
+											@if($sale != "")
+											<span class="price"><span class="price-sale">${{$value->price()->sale}} USD</span> 
+											<span class="old-price"> ${{$value->price()->price}} USD</span></span>
+											@else
+											<span class="price">FROM <span>${{$value->price()->price}} USD</span></span>
+											
+											@endif
 										</div>
+										@if($sale != "")
 										<div class="tag-stt">
 											<span>On sale</span>
 										</div>
+										@endif
 									</div>
 								</div>
-								<div class="col-md-4 col-sm-4 col-xs-12">
-									<div class="product-item">
-										<div class="pro-img">
-											<a href="#">
-												<img src="https://cdn.shopify.com/s/files/1/0250/1519/products/esqido-unisyn-lashes-peace_love_400x.jpg?v=1551684701" alt="">
-												<div class="img-hide">
-													<img src="https://cdn.shopify.com/s/files/1/0250/1519/products/esq-unisyn-false-eyelashes-peace-and-love_61716f75-d011-421a-94ff-fb9bab042d0f_300x.jpg?v=1551684702" alt="">
-												</div>
-											</a>
-										</div>
-										<div class="info-product">
-											<h3 class="title-pro"><a href="#">COMPANION EYELASH GLUE</a></h3>
-											<span class="price">FROM <span>$22.00 USD</span></span>
-										</div>
-										<div class="tag-stt">
-											<span>Sold out</span>
-										</div>
-									</div>
-								</div>
-								<div class="col-md-4 col-sm-4 col-xs-12">
-									<div class="product-item">
-										<div class="pro-img">
-											<a href="#">
-												<img src="https://cdn.shopify.com/s/files/1/0250/1519/products/esqido-accessories-eyelash-companion-glue-open_400x.jpg?v=1552317290" alt="">
-												<div class="img-hide">
-													<img src="https://cdn.shopify.com/s/files/1/0250/1519/products/esqido-accessories-eyelash-companion-glue_900x.jpg?v=1552317290" alt="">
-												</div>
-											</a>
-										</div>
-										<div class="info-product">
-											<h3 class="title-pro"><a href="#">COMPANION EYELASH GLUE</a></h3>
-											<span class="price">FROM <span>$22.00 USD</span></span>
-										</div>
-										<div class="tag-stt">
-											<span>On sale</span>
-										</div>
-									</div>
-								</div>
+								@endforeach
+								
 							</div>
 						</div>
 					</div>
