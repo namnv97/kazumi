2ad93571e3cf966e61a19f2153d685f8fbe54860
namnv97 <n4m.nv.1997@gmail.com>
2019-09-18 09:46:39 +0700

    update share messenger

--- resources/views/client/account/reward/ref.blade.php
+++ resources/views/client/account/reward/ref.blade.php
@@ -16,7 +16,7 @@
 			<div class="lion-referral-widget-main__share-buttons">
 				<a href="https://twitter.com/intent/tweet?url={{route('home',['ref' => Auth::user()->refferal_code])}}&text=Tham gia ngay với tôi&via={{route('home')}}&hashtags=Kazumi" target="_blank" class="lion-referral-share-button lion-referral-share-button--twitter"><i class="fa fa-twitter" aria-hidden="true"></i> TWEET </a>
 				<a href="#" class="lion-referral-share-button lion-referral-share-button--whatsapp" target="_blank"><i class="fa fa-whatsapp" aria-hidden="true"></i> whatsapp </a>
-				<a href="#" class="lion-referral-share-button lion-referral-share-button--facebook-messenger">messenger </a>
+				<a href="http://www.facebook.com/dialog/send?app_id=381461119271778&amp;link={{route('home',['ref' => Auth::user()->refferal_code])}}&amp;redirect_uri={{route('home')}}&amp;display=popup" class="lion-referral-share-button lion-referral-share-button--facebook-messenger">messenger </a>
 				<a href="https://www.facebook.com/sharer.php?u={{route('home',['ref' => Auth::user()->refferal_code])}}" class="lion-referral-share-button lion-referral-share-button--facebook" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i> SHARE </a>
 				<a href="#" class="lion-referral-share-button lion-referral-share-button--email" target="_blank"><i class="fa fa-envelope" aria-hidden="true"></i> email </a>
 			</div>
